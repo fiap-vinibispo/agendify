5c83367abdad20a604ae370a1c2cb8e8315a3776
vinibispo <vini.bispo015@gmail.com>
2024-11-30 20:40:07 -0300

    Finish landing page

---
+++ src/components/form.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("section#registration form")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import "./components/menu.js"
 import "./components/image-with-text.js"
+import "./components/form.js"
